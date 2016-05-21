235b9917ff9b66dc2cc89cbcc328fadffb403644
Mitchell Cox <mitchell.allan.cox@gmail.com>
2016-05-21 12:25:19 -0600

    Home updates

--- modules/components/Home.js
+++ modules/components/Home.js
@@ -6,7 +6,28 @@ export default React.createClass({
     return (
       <div>
         <Title render={prev => `${prev} | Home`}/>
-        <p>Drink</p>
+        <body>
+          <div className="parallax-container">
+            <div className="parallax"><img src="../../static/images/wood-textures.jpg" /> </div>
+          </div>
+
+          <div className="section white">
+            <div className="row container">
+              <form>
+                <div className="input-field">
+                  <input id="search" type="search" required />
+                  <label for="search"><i className="material-icons" placeholder="What ingredients do you have?"></i></label>
+                  <i className="material-icons">close</i>
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <div className="parallax-container">
+            <div className="parallax"><img src="../../static/images/wood-textures.jpg" /> </div>
+          </div>
+          
+        </body>
       </div>
     )
   }
